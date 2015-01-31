1cb207db36f7ae76130c30b8166c31593f14da5b
karthik reddy acer <karthik.gakkidi@gmail.com>
2015-01-31 17:13:32 -0500

    strcmp implemented

--- bin/sbush/shell.c
+++ bin/sbush/shell.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char *argv[]){
-  
+int strcmp(const char *s1,const char *s2);
+
+int main(int argc, char *argv[],char *envp[]){
+  printf("hello\n");
+  //printf("%d\n",strcmp("cd","cd"));
+  return 1;
+}
+
+int strcmp(const char *s1,const char * s2){
+  int i=0;
+  for(i=0;s1[i]==s2[i];i++){
+    if(s1[i]=='\0')
+      return 0;
+  }
+  return s1[i]-s2[i];
 }
