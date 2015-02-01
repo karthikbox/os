76592baedad55722f3d40ac2d86f14b3c669c22b
karthik reddy acer <karthik.gakkidi@gmail.com>
2015-01-31 21:34:20 -0500

    cd works

--- bin/sbush/shell.c
+++ bin/sbush/shell.c
@@ -2,30 +2,60 @@
 #include<stdlib.h>
 
 int strcmp(const char *s1,const char *s2);
-char** strtoken(const char *s, const char *delim);
+char** strtoken(const char *s, const char *delim,int *len);
+void cmd_cd(char** tokens);
+void cmd_set(char** tokens);
+void printPrompt(char* str);
 
 int main(int argc, char *argv[],char *envp[]){
   	char name[1000];
-  
+	int token_len;
+	int i;
+	//char path
+	char prompt[500];
+	char *prompt_ret;
   	while(1)
   	{
-
-	  	printf("sbush@cse506$ ");
+	  //printf("sbush@cse506$ ");
+	  prompt_ret =getcwd(prompt,sizeof(prompt)+1);
+	  if(prompt_ret!=0)
+		printPrompt(prompt);
 	  	scanf(" %[^\n]s", name);
 	  	if(strcmp(name, "exit") == 0)
 	  	{
 	  		break;
 	  	}
 
-	  	char** tokens = strtoken(name, " ");
-	  	int i = 0;
-
-	  	while(tokens[i] != 0){
-	  		printf("%s\n", tokens[i]);
-	  		i++;
-  		}
+	  	char** tokens = strtoken(name, " ",&token_len);
+	  	i = 0;
+		if(strcmp(tokens[i],"cd")==0){
+		  if(token_len==2)
+		    cmd_cd(tokens);
+		  else
+		    printf("incorrect syntax for cd\n");
+		}
+		else if(strcmp(tokens[i],"set")==0){
+		  if(token_len==2||token_len==3){
+		    cmd_set(tokens);
+		  }
+		  else
+		    printf("incorrect syntax for cd\n");
+		}
 	}
   	return 1;
+
+}
+
+void printPrompt(char* str){
+  printf("%s$ ",str);
+}
+
+void cmd_cd(char** tokens){
+  if(chdir(tokens[1])<0)
+    printf("error\n");
+}
+
+void cmd_set(char** tokens){
 
 }
 
@@ -38,32 +68,32 @@ int strcmp(const char *s1,const char * s2){
 	return s1[i]-s2[i];
 }
 
-char** strtoken(const char *s, const char *delim){
-int i=0, j=0, k=0;
-char **tokens = (char**)malloc(100*sizeof(char*));
+char** strtoken(const char *s, const char *delim,int *len){
+  int i=0, j=0, k=0;
+  char **tokens = (char**)malloc(100*sizeof(char*));
 
-tokens[j] = (char*)malloc(200*sizeof(char));
-
-
-
-while(s[i] != '\0'){
-	if(s[i] == *delim)
-	{
-		tokens[j][k] = '\0';
-		j++;
-		tokens[j] = (char*)malloc(200*sizeof(char));
-		k=0;
-	}
-	else
-	{
-		tokens[j][k] = s[i];
-		k++;			
-	}
-	i++;
-}
-tokens[j][k] = '\0';
-tokens[++j] = 0;
+  tokens[j] = (char*)malloc(200*sizeof(char));
 
 
-return tokens;
+
+  while(s[i] != '\0'){
+    if(s[i] == *delim)
+      {
+	tokens[j][k] = '\0';
+	j++;
+	tokens[j] = (char*)malloc(200*sizeof(char));
+	k=0;
+      }
+    else
+      {
+	tokens[j][k] = s[i];
+	k++;			
+      }
+    i++;
+  }
+  tokens[j][k] = '\0';
+  tokens[++j] = 0;
+  *len=j;
+
+  return tokens;
 }
