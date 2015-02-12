086b03e015d573fcf5c0202ecca9a2d846e07f4a
Harsha <harsha049@gmail.com>
2015-02-12 01:16:54 -0500

    crt1 file..issue with %s. need to check how to input it.

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,10 +1,30 @@
 #include <stdlib.h>
+#include<stdio.h>
+uint64_t *get_rsp();
 
 void _start(void) {
-	int argc = 1;
+	uint64_t count,i;
 	char* argv[0];
 	char* envp[0];
 	int res;
-	res = main(argc, argv, envp);
+	uint64_t *rsp;
+	__asm__
+	(
+		"mov %%rsp,%0\n\t"
+		:"=r" (rsp)
+	);
+//	printf("1) %x\n",*rsp);
+	printf("2) %x\n",*(rsp+5));
+	rsp=rsp+5;	//rsp+1 implies after 8 bits we have argc
+	printf("3) %x\n",*rsp);
+	count=*rsp;
+	i=1;
+	while(i<count){
+		printf("argc %s\n",*(rsp+i));
+		i++;
+	}
+	res = main(*rsp, argv, envp);
 	exit(res);
 }
+
+
