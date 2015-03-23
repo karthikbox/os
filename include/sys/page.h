625c4f20a42e945524fa4e5891187d8828956ff2
Karthik Reddy <kgreddy@cs.stonybrook.edu>
2015-03-23 14:48:30 -0400

    vm_init implemented. vm_map_page in progress. made all preprocesssor functions as inline for better warning from compiler.

--- include/sys/page.h
+++ include/sys/page.h
@@ -3,20 +3,23 @@
 
 #include <sys/defs.h>
 
+
+
+
 /* level 1 */
 enum PAGE_PTE_FLAGS{
 
-	PTE_PRESENT = 1,//1
-	PTE_WRITABLE = 2,
-	PTE_USER = 4,//3
-	PTE_WRITETHROUGH =8,
-	PTE_NOT_CACHEABLE=0x10,
-	PTE_ACCESSED=0x20,//6
-	PTE_DIRTY=0x40,
-	PTE_NULL=0x80,//8
-	PTE_CPU_GLOBAL=0x100,
-	PTE_LV4_GLOBAL=0x200,//10
-	PTE_FRAME=0x0000FFFFFFFFF000//36 bit frame number [12-47]
+	PTE_PRESENT = 1ul,//1
+	PTE_WRITABLE = 2ul,
+	PTE_USER = 4ul,//3
+	PTE_WRITETHROUGH =8ul,
+	PTE_NOT_CACHEABLE=0x10ul,
+	PTE_ACCESSED=0x20ul,//6
+	PTE_DIRTY=0x40ul,
+	PTE_NULL=0x80ul,//8
+	PTE_CPU_GLOBAL=0x100ul,
+	PTE_LV4_GLOBAL=0x200ul,//10
+	PTE_FRAME=0x0000FFFFFFFFF000ul//36 bit frame number [12-47]
 };
 
 /*page table entry */
@@ -28,21 +31,21 @@ void pt_entry_set_frame(pt_entry *e, uint64_t addr);
 int pt_entry_is_present(pt_entry e);
 int pt_entry_is_writable(pt_entry e);
 int pt_entry_is_user(pt_entry e);
-uint64_t pt_entry_pfn(pt_entry e);
+uint64_t pt_entry_get_frame(pt_entry e);
 
 enum PAGE_PD_FLAGS{
 
-	PD_PRESENT = 1,//1
-	PD_WRITABLE = 2,
-	PD_USER = 4,//3
-	PD_WRITETHROUGH =8,
-	PD_NOT_CACHEABLE=0x10,
-	PD_ACCESSED=0x20,//6
-	PD_DIRTY=0x40,
-	PD_NULL=0x80,//8
-	PD_CPU_GLOBAL=0x100,
-	PD_LV4_GLOBAL=0x200,//10
-	PD_FRAME=0x0000FFFFFFFFF000//36 bit frame number [12-47]
+	PD_PRESENT = 1ul,//1
+	PD_WRITABLE = 2ul,
+	PD_USER = 4ul,//3
+	PD_WRITETHROUGH =8ul,
+	PD_NOT_CACHEABLE=0x10ul,
+	PD_ACCESSED=0x20ul,//6
+	PD_DIRTY=0x40ul,
+	PD_NULL=0x80ul,//8
+	PD_CPU_GLOBAL=0x100ul,
+	PD_LV4_GLOBAL=0x200ul,//10
+	PD_FRAME=0x0000FFFFFFFFF000ul//36 bit frame number [12-47]
 };
 
 typedef uint64_t pd_entry;
@@ -54,7 +57,7 @@ int pd_entry_is_present(pd_entry e);
 int pd_entry_is_writable(pd_entry e);
 int pd_entry_is_user(pd_entry e);
 int pd_entry_is_4mb(pd_entry e);
-uint64_t pd_entry_pfn(pd_entry e);
+uint64_t pd_entry_get_frame(pd_entry e);
 int pd_entry_is_enable_global(pd_entry e);
 
 
@@ -64,35 +67,39 @@ int pd_entry_is_enable_global(pd_entry e);
 #define ENTRIES_PER_PDP 512
 #define ENTRIES_PER_PML4 512
 
-#define PML4_INDEX(x) ((x>>39) & 0x1ff) /* 9 bits */
-#define PDP_INDEX(x) ((x>>30) & 0x1ff)
-#define PD_INDEX(x) ((x>>21) & 0x1ff)
-#define PT_INDEX(x) ((x>>12) & 0x1ff)
-#define LOWER_OFFSET(x) (x & 0xfff) /* 12 bits */
-
+inline uint64_t pml4_index(uint64_t x);
+inline uint64_t pdp_index(uint64_t x);
+inline uint64_t pd_index(uint64_t x);
+inline uint64_t pt_index(uint64_t x);
+inline uint64_t lower_offset(uint64_t x);
 
 #define PAGE_SIZE 4096
 /* pd, pdp, pml4 are ientical. so pd_entry holds for the three of these */
 /* pt_entry holds pt entries, since it is slightly different from the above */
-struct pt{
+typedef struct{
 	pt_entry m_entries[ENTRIES_PER_PT];
-};
+}pt;
 
-struct pd{
+typedef struct{
 	pd_entry m_entries[ENTRIES_PER_PD];
-};
+}pd;
 
-struct pdp{
+typedef struct{
 	pd_entry m_entries[ENTRIES_PER_PDP];
-};
+}pdp;
 
-struct pml4{
+typedef struct{
 	pd_entry m_entries[ENTRIES_PER_PML4];
-};
+}pml4;
+
+
+/* holds the base address of pml4 frame */
+pml4 *pml4_base;
 
 int vm_alloc_page(pt_entry* e);
 void vm_free_page(pt_entry* e);
-void mv_map_page(void* phys,coid *virt);
+int vm_page_map(uint64_t phys,uint64_t virt,int rx_bit);
+void load_base(uint64_t addr);
 /*
 1234 5678 
 0000 0000 1 00
--- sys/page.c
+++ sys/page.c
@@ -1,6 +1,98 @@
 #include<sys/page.h>
+#include<sys/pmmgr.h>
+#include<sys/utility.h>
+#include<sys/sbunix.h>
+
+int vm_init(void* physbase,void* physfree){
+	/* alloc 1 frame and clear it for pml4 */
+	pml4_base=alloc_frame();
+	
+	if(!pml4_base){
+		printf("unable to allocate frame\n");
+		return -1;
+	}
+	memset1((char *)pml4_base,0,FRAME_SIZE);
+	
+	/* for page from 0 to 4KB
+	   map each page identically*/
+	uint64_t i=0;
+	for(i=0;i<0x100000ul;i+=0x1000ul){ /* for every page till 1MB */
+		if(vm_page_map(i,i,1)==-1){		/* identity map */
+			printf("unable to map\n");
+			return -1;
+		}
+	}
+	
+	/* kernmem = 0xffffffff80000000 + physbase */
+	/* for each page from kernmem to kernfree-1 */
+	/* map page to range physbase, physfree-1 */
+	/* physbase if */
+	for(i=(uint64_t)physbase;i<(uint64_t)physfree;i+=0x1000ul){
+		if(vm_page_map(i,0xffffffff80000000ul+i,1)==-1){	/* physical addr, virt addr */
+			printf("unable to map\n");
+			return -1;
+		}
+	}
+	
+	/* load pml4_base into crx register. */
+	/* 	this will also invalidate tlb */
+	load_base((uint64_t)pml4_base);
+	return 0;
+}
 
 
-void vm_map_page(void *phys,void *virt){
+inline uint64_t pml4_index(uint64_t x){
+	return ((x>>39) & 0x1fful);
+}
+
+inline uint64_t pdp_index(uint64_t x){
+	return ((x>>30) & 0x1fful);
+}
+
+inline uint64_t pd_index(uint64_t x){
+	return ((x>>21) & 0x1fful);
+}
+
+inline uint64_t pt_index(uint64_t x){
+	return ((x>>12) & 0x1fful);
+}
+
+inline uint64_t lower_offset(uint64_t x){
+	return ( x & 0x0ffful);
+}
+
+inline int pd_entry_present(pd_entry e){
+	return (e & PD_PRESENT);			/* returns 1 if P is set, otherwise 0 */
+}
+
+inline uint64_t pd_entry_get_frame(pd_entry e){
+	return (e & PD_FRAME);			
+}
+
+inline void pd_entry_set_frame(pd_entry *e,uint64_t addr){
+	
+	*e = addr | (*e & 0xffful); /* the first 12 bits of addr will be 0 since addr is 4KB aligned */
 	
 }
+
+
+int vm_page_map(uint64_t phys_addr,uint64_t virt_addr,int rx_bit){
+
+	/* maps phys_addr to virt_addr */
+	/* phys_addr will be base addr of frame */
+	/* rx_bit is 1, so that page can be RW  */
+	
+	//uint64_t pdp_offset=pml4_index(virt_addr);
+	
+
+
+
+	return -1;
+	
+}
+
+
+
+ void load_base(uint64_t addr){
+	 
+ }
