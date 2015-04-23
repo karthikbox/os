0dd1e345918c279e3ba05bc1fcb1ef442d3d00ff
Satya_Hara_Pavan_Kumar Maguluri <smaguluri@compute-3-16.local>
2015-04-23 02:23:56 -0400

    modified sys main.c

--- sys/main.c
+++ sys/main.c
@@ -76,11 +76,22 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 	/* printf("one again: %p",one); */
 	/* int* five=(int*)kmalloc(4096); */
 	/* printf("five:last+1000 %p",five); */
-	int* num; 
-	for(int i=0; i<205; i++) {
-		num = (int*)kmalloc(sizeof(int)); 
-		printf("num is %p\n", num); 
+	int* num;
+	for(int i=0; i<204; i++) {
+		num = (int*)kmalloc(sizeof(int));
+		*num = i;
+		printf("num is %d\n", *num);
 	}
+
+	num = (int*)kmalloc(sizeof(int));
+	num = (int*)kmalloc(sizeof(int));
+	num = (int*)kmalloc(sizeof(int));
+	num = (int*)kmalloc(sizeof(int));
+	num = (int*)kmalloc(sizeof(int));
+	num = (int*)kmalloc(sizeof(int));
+
+
+	
 	//__asm__ __volatile__("int $0x00");
 	//__asm__ __volatile__("int $0x80");
 	/* uint64_t *elf_start; */
