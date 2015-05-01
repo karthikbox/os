0e03d9f5992a5de3564b17b8d41bcd3ceec68241
Satya_Hara_Pavan_Kumar Maguluri <smaguluri@compute-3-16.local>
2015-04-30 21:06:10 -0400

    moved string functions to strings.c file

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,52 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
-
-int strcmp(const char *s1,const char * s2) {
-    int i=0;
-    for(i=0; s1[i]==s2[i]; i++) {
-        if(s1[i]=='\0')
-            return 0;
-    }
-    return s1[i]-s2[i];
-}
-
-
-
-int strlen(char *s)
-{
-    int i = 0;
-    while(s[i] != '\0')
-        i++;
-    return i;
-
-}
-
-void strcat(char* s1, char* s2)
-{
-    int i=0, j=0;
-    while (s1[i] != '\0')
-    {
-        i++;
-    }
-    while (s2[j] != '\0')
-    {
-        s1[i] = s2[j];
-        i++;
-        j++;
-    }
-    s1[i] = '\0';
-}
-
-void strcpy(char* dest, char* src)
-{
-    int i=0;
-    while(src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-}
+#include<string.h>
 
 
 int main(int argc, char* argv[], char* envp[]) {
--- include/string.h
+++ include/string.h
@@ -5,5 +5,7 @@
 
 int strcmp(const char *s1,const char *s2);
 char** strtoken(const char *s, const char *delim,int *len);
-
+int strlen(char *s);
+void strcat(char* s1, char* s2);
+void strcpy(char* dest, char* src);
 #endif
