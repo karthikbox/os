ddd0432ae62517bd9b0cb3a7cd56f6670260a05c
Satya_Hara_Pavan_Kumar Maguluri <smaguluri@compute-3-16.local>
2015-05-05 05:32:09 -0400

    kill -9 utility modified, supports only a single process now

--- bin/kill/kill.c
+++ bin/kill/kill.c
@@ -4,21 +4,24 @@
 
 int main(int argc, char* argv[], char* envp[]){
 	int pid=0;
-	int i;
+
 	if(argc<3){
 		printf("kill: Too few arguments\n");
 		return 0;
 	}
+
+	if(argc>3){
+		printf("kill: Too many arguments\n");
+	}
+
 	if(strcmp(argv[1],"-9")!=0){
 		printf("Usage: kill -9 <pid>\n");
 		return 0;
 	}
 
-	for(i=2;i<argc;i++){
-		atoi(argv[i], &pid, 10);
-		if(kill(pid)==-1){
-			printf("%s: No such process\n", argv[i]);
-		}
+	atoi(argv[2], &pid, 10);
+	if(kill(pid)==-1){
+		printf("%s: No such process\n", argv[2]);
 	}
 	return 0;
 }
