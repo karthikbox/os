4f556fdd7484aa578e74fd4c629ef1a479dddecd
Satya_Hara_Pavan_Kumar Maguluri <smaguluri@compute-3-16.local>
2015-05-06 01:52:02 -0400

    added kmalloc.c, binary for testing kmalloc. added start.sh

---
+++ start.sh
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -curses -cdrom $USER.iso -drive id=disk,file=$USER.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -net nic -net user,hostfwd=tcp::10080-:80 -net user,hostfwd=tcp::10023-:23 --no-reboot -gdb tcp::9998
